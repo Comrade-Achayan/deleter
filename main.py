77800f0c5b31ac8dce6a0af000b26151f1593f98
꧁☬⋆cσɱɾα∂ε༒αcɦαყαɳ⋆☬꧂ <70142311+Comrade-Achayan@users.noreply.github.com>
2022-04-12 18:56:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,29 +14,19 @@ Bot = Client(
 
 @Bot.on_message(filters.group & (filters.text | filters.reply | filters.photo))
 async def dlt(bot, update):
-   # await update.reply(text="")
-    await asyncio.sleep(120)
+    await asyncio.sleep(60)
     await update.delete()
-   # await bot.delete_messages(
-  #           chat_id=update.chat_id,
-    #         message_ids=update.message_id
- #   )
 
-@Bot.on_message(filters.group & filters.bot & (filters.text | filters.reply | filters.photo))
+@Bot.on_message(filters.group & filters.bot & filters.admin)
 async def det(bot, update):
     await asyncio.sleep(120)
     await update.delete()
 
 @Bot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
-    
-   # if not await db.is_user_exist(update.from_user.id):
-	   # await db.add_user(update.from_user.id)
-    
     await update.reply_text(
         text="hi",         #START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
-	#reply_markup=START_BUTTONS
     )
 
 Bot.run()
