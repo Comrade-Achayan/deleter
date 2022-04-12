fa8d073693901bc119b61e4d4ca959ff26d41b44
꧁☬⋆cσɱɾα∂ε༒αcɦαყαɳ⋆☬꧂ <70142311+Comrade-Achayan@users.noreply.github.com>
2022-04-12 12:40:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import asyncio
 import os
 from pyrogram import Client, filters
 
+AUTH_GROUP = os.environ["AUTH_GROUP"],
+
 Bot = Client(
     "Deleter",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
 )
+
+@Bot.on_message(filters.chat(AUTH_GROUP) & (filters.text | filters.photo))
+async def dlt(bot, update):
+    mgd = update.chat.title
+    await asyncio.sleep(120)
+    await update.delete()
+
+
+Bot.run
